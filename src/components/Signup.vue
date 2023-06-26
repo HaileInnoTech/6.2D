7ef228f06a323033a78e20fefeef0044905c7a40
HellCursed2u <hai28022002@gmail.com>
2023-06-26 16:07:50 +0700

    Update Signup.vue

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container mx-auto">
     <div class="col-6 mx-auto bg-main height">
+
       <div class="row">
-        <img
-          src="../images/Screenshot_2023-06-23_115513-transformed.png"
-          style="width: 65%; height: auto"
-          class="rounded mx-auto d-block"
-          alt="..."
-        />
+        <img src="../images/Screenshot_2023-06-23_115513-transformed.png" style="width: 50%; height: auto"
+          class="rounded mx-auto d-block" alt="..." />
       </div>
 
       <div class="row">
@@ -17,60 +14,51 @@
       <div class="row">
         <form>
           <div class="mb-3 px-5">
-            <label for="exampleInputEmail1" class="form-label"
-              >Email address</label
-            >
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
+            <label for="fullname" class="form-label">Full name</label>
+            <input type="fullname" class="form-control" id="fullname" v-model="fullname" />
           </div>
           <div class="mb-3 px-5">
-            <label for="exampleInputPassword1" class="form-label"
-              >Password</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
+            <label for="email" class="form-label">Email </label>
+            <input type="text" class="form-control" id="email" v-model="email" />
           </div>
           <div class="mb-3 px-5">
-            <label for="exampleInputPassword1" class="form-label"
-              >Password</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="passwork" v-model="password" />
           </div>
           <div class="position-relative mt-5">
-            <button
-              type="submit"
-              class="btn btn-primary btn-lg position-absolute top-50 start-50 translate-middle"
-            >
+            <button type="submit" class="btn btn-primary btn-lg position-absolute top-50 start-50 translate-middle">
               Sign up
             </button>
           </div>
         </form>
       </div>
-      <span></span>
+
+      <div class="row mt-5 text-center">
+        <p>I'm already a member</p>
+        <span>Sign In</span> // router link
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      fullname: '',
+      email: '',
+      password: ''
+    }
+  }
+};
 </script>
 
 <style>
 .bg-main {
   background-color: #edf9ff;
 }
+
 .height {
-  height: 650px;
+  height: 700px;
 }
 </style>
